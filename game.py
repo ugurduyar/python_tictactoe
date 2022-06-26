3ddc2e59459286ecc0e0e6df442309479cb15af4
ugurduyar <ismailugurduyar@gmail.com>
2022-06-26 20:38:05 +0300

    print board

---
+++ game.py
@@ -0,0 +1,12 @@
+class tic_tac_toe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+        self.current_winner = None
+    
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        
